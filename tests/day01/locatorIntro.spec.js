263f7ba53c6d5e9f2de4730d1288d8b857d1b63e
Usman Karim <muhammadkarim1312@gmail.com>
2025-11-15 15:56:25 -0500

    deleted some files

--- tests/day01/locatorIntro.spec.js
+++
@@ -1,8 +0,0 @@
-import { test, expect } from '@playwright/test';
-test('locator', async ({ page }) => {
-    await page.goto('https://www.google.com');
-    let searchBox = page.locator("textarea[id='APjFqb']");
-    await searchBox.fill("Playwright Locators");
-    await searchBox.press('Enter');
-
-})
